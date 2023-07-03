16cb91adc8ca33e8db9738bd84fc7821287d7661
James777G <hgon777@aucklanduni.ac.nz>
2023-07-03 23:50:25 +1200

    @data

--- src/main/java/com/healthtrack/calculator/domain/UserInfo.java
+++ src/main/java/com/healthtrack/calculator/domain/UserInfo.java
@@ -1,9 +1,14 @@
 package com.healthtrack.calculator.domain;
 
+import lombok.Data;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
 
+@Data
 public class UserInfo implements Serializable {
+    @Serial
     private static final long serialVersionUID = 55736572496666L;
 
     private int userId;
@@ -14,61 +19,7 @@ public class UserInfo implements Serializable {
     private int weight;
     private HashSet<String> symptoms;
 
-    public int getUserId() {
-        return userId;
-    }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getSex() {
-        return sex;
-    }
-
-    public void setSex(int sex) {
-        this.sex = sex;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public HashSet<String> getSymptoms() {
-        return symptoms;
-    }
-
-    public void setSymptoms(HashSet<String> symptoms) {
-        this.symptoms = symptoms;
-    }
 
 }
 
